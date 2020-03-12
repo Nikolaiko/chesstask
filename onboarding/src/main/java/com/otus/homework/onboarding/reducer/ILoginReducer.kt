e7fab80b419c18dc1812ae209362c19e4098c8d7
Nikolay Baklanov <>
2020-03-12 16:21:52 +0600

    Style

--- onboarding/src/main/java/com/otus/homework/onboarding/reducer/ILoginReducer.kt
+++ onboarding/src/main/java/com/otus/homework/onboarding/reducer/ILoginReducer.kt
@@ -7,12 +7,12 @@ import com.otus.homework.onboarding.model.enums.OnBoardingScreens
 import io.reactivex.subjects.PublishSubject
 
 interface ILoginReducer {
-    val updateState:PublishSubject<LoginState>
-    val updateNews:PublishSubject<OnBoardingNews>
-    val updateDestination:PublishSubject<OnBoardingScreens>
+    val updateState: PublishSubject<LoginState>
+    val updateNews: PublishSubject<OnBoardingNews>
+    val updateDestination: PublishSubject<OnBoardingScreens>
 
-    fun credentialsChange(userData: UserProfile):LoginState
-    fun tryToLogin():LoginState
-    fun register():LoginState
+    fun credentialsChange(userData: UserProfile): LoginState
+    fun tryToLogin(): LoginState
+    fun register(): LoginState
     fun clearDisposables()
 }
